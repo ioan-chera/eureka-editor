7fae899461d29b812c8648de9adceb6638f921b4
Andrew Apted <ajapted@users.sf.net>
2013-04-25 12:39:59 +0000

    Silage: added code to invoke glBSP on the output wadfile.

--- misc/silage/main.cc
+++ misc/silage/main.cc
@@ -85,12 +85,6 @@ static void Main_OpenWindow()
 #endif
 
 
-	int screen_w = Fl::w();
-	int screen_h = Fl::h();
-
-//	DebugPrintf("Detected Screen Size: %dx%d\n", screen_w, screen_h);
-
-
 	main_win = new UI_MainWin("Silage v" SILAGE_VERSION);
 
 	// show window (pass some dummy arguments)
@@ -120,6 +114,193 @@ static void Main_CloseWindow()
 		delete main_win;
 	
 	main_win = NULL;
+}
+
+
+
+//------------------------------------------------------------------------
+
+#include "glbsp.h"
+
+static glbsp::nodebuildinfo_t nb_info;
+static volatile glbsp::nodebuildcomms_t nb_comms;
+
+static int display_mode = glbsp::DIS_INVALID;
+static int progress_limit;
+
+static char message_buf[MSG_BUF_LEN];
+
+
+static const char *glbsp_ErrorString(glbsp::glbsp_ret_e ret)
+{
+	switch (ret)
+	{
+		case glbsp::GLBSP_E_OK: return "OK";
+
+		 // the arguments were bad/inconsistent.
+		case glbsp::GLBSP_E_BadArgs: return "Bad Arguments";
+
+		// the info was bad/inconsistent, but has been fixed
+		case glbsp::GLBSP_E_BadInfoFixed: return "Bad Args (fixed)";
+
+		// file errors
+		case glbsp::GLBSP_E_ReadError:  return "Read Error";
+		case glbsp::GLBSP_E_WriteError: return "Write Error";
+
+		// building was cancelled
+		case glbsp::GLBSP_E_Cancelled: return "Cancelled by User";
+
+		// an unknown error occurred (this is the catch-all value)
+		case glbsp::GLBSP_E_Unknown:
+
+		default: return "Unknown Error";
+	}
+}
+
+
+static void GB_PrintMsg(const char *str, ...)
+{
+	va_list args;
+
+	va_start(args, str);
+	vsnprintf(message_buf, MSG_BUF_LEN, str, args);
+	va_end(args);
+
+	message_buf[MSG_BUF_LEN-1] = 0;
+
+//??	LogPrintf("GLBSP: %s", message_buf);
+}
+
+static void GB_FatalError(const char *str, ...)
+{
+	va_list args;
+
+	va_start(args, str);
+	vsnprintf(message_buf, MSG_BUF_LEN, str, args);
+	va_end(args);
+
+	message_buf[MSG_BUF_LEN-1] = 0;
+
+	FatalError("glBSP Failure:\n%s", message_buf);
+	/* NOT REACHED */
+}
+
+static void GB_Ticker(void)
+{
+	if (want_build /* is now a "cancel" button */)
+	{
+		nb_comms.cancelled = TRUE;
+	}
+}
+
+static glbsp::boolean_g GB_DisplayOpen(glbsp::displaytype_e type)
+{
+	display_mode = type;
+	return TRUE;
+}
+
+static void GB_DisplaySetTitle(const char *str)
+{
+	/* does nothing */
+}
+
+static void GB_DisplaySetBarText(int barnum, const char *str)
+{
+	if (display_mode == glbsp::DIS_BUILDPROGRESS && barnum == 1)
+	{
+		// needed ??
+	}
+}
+
+static void GB_DisplaySetBarLimit(int barnum, int limit)
+{
+	if (display_mode == glbsp::DIS_BUILDPROGRESS && barnum == 2)
+	{
+		progress_limit = MAX(1, limit);
+	}
+}
+
+static void GB_DisplaySetBar(int barnum, int count)
+{
+	if (display_mode == glbsp::DIS_BUILDPROGRESS && barnum == 2)
+	{
+		int perc = count * 100.0 / progress_limit;
+
+		// FIXME: main_win->SetProgress(perc);
+	}
+}
+
+static void GB_DisplayClose(void)
+{
+	/* does nothing */
+}
+
+static const glbsp::nodebuildfuncs_t  build_funcs =
+{
+	GB_FatalError,
+	GB_PrintMsg,
+	GB_Ticker,
+
+	GB_DisplayOpen,
+	GB_DisplaySetTitle,
+	GB_DisplaySetBar,
+	GB_DisplaySetBarLimit,
+	GB_DisplaySetBarText,
+	GB_DisplayClose
+};
+
+
+static bool Main_RunGLBSP(const char *filename)
+{
+	display_mode = glbsp::DIS_INVALID;
+
+	memcpy(&nb_info,  &glbsp::default_buildinfo,  sizeof(glbsp::default_buildinfo));
+	memcpy((void*)&nb_comms, &glbsp::default_buildcomms, sizeof(glbsp::nodebuildcomms_t));
+
+	nb_info.input_file  = glbsp::GlbspStrDup(filename);
+	nb_info.output_file = glbsp::GlbspStrDup(filename);
+
+	nb_info.fast          = FALSE;
+	nb_info.quiet         = TRUE;
+	nb_info.mini_warnings = FALSE;
+
+	nb_info.pack_sides = FALSE;
+	nb_info.force_normal = TRUE;
+
+
+	glbsp::glbsp_ret_e  ret;
+
+	ret = glbsp::CheckInfo(&nb_info, &nb_comms);
+
+	if (ret != glbsp::GLBSP_E_OK)
+	{
+		// check info failure (unlikely to happen)
+		GB_PrintMsg("\n");
+		GB_PrintMsg("Param Check FAILED: %s\n", glbsp_ErrorString(ret));
+		GB_PrintMsg("Reason: %s\n\n", nb_comms.message);
+		return false;
+	}
+
+
+	ret = glbsp::BuildNodes(&nb_info, &build_funcs, &nb_comms);
+
+	if (ret == glbsp::GLBSP_E_Cancelled)
+	{
+		GB_PrintMsg("\n");
+		GB_PrintMsg("Building CANCELLED.\n\n");
+		return false;
+	}
+
+	if (ret != glbsp::GLBSP_E_OK)
+	{
+		// build nodes failed
+		GB_PrintMsg("\n");
+		GB_PrintMsg("Building FAILED: %s\n", glbsp_ErrorString(ret));
+		GB_PrintMsg("Reason: %s\n\n", nb_comms.message);
+		return false;
+	}
+
+	return true;
 }
 
 
@@ -164,6 +345,8 @@ static void Main_Build()
 
 
 fprintf(stderr, "BUILD FILENAME = '%s'\n", filename);
+
+	Main_RunGLBSP(filename);
 }
 
 
@@ -184,10 +367,6 @@ static void ShowHelp()
  */
 int main(int argc, char *argv[])
 {
-	init_progress = 0;
-
-	int r;
-
 	/* TODO: argument handling */
 
 	if (argc >= 2 &&
@@ -220,7 +399,6 @@ int main(int argc, char *argv[])
     }
 
 
-quit:
 	/* that's all folks! */
 
 
