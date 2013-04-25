d8caa8101d43591ad1055127846a731c0e2a4ce3
Andrew Apted <ajapted@users.sf.net>
2013-04-25 11:17:22 +0000

    Silage: added bare-bones main program code : main.cc / h

---
+++ misc/silage/main.cc
@@ -0,0 +1,191 @@
+//------------------------------------------------------------------------
+//  MAIN PROGRAM
+//------------------------------------------------------------------------
+
+#include "main.h"
+
+
+/*
+ *  Global variables
+ */
+
+// progress during initialisation:
+//   0 = nothing yet
+//   1 = read early options, set up logging
+//   2 = read all options, and possibly a config file
+//   3 = inited FLTK, opened main window
+int  init_progress;
+
+
+static void Main_CloseWindow();
+
+
+/*
+ *  Show an error message and terminate the program
+ */
+void FatalError(const char *fmt, ...)
+{
+	va_list arg_ptr;
+
+	static char buffer[MSG_BUF_LEN];
+
+	va_start(arg_ptr, fmt);
+	vsnprintf(buffer, MSG_BUF_LEN-1, fmt, arg_ptr);
+	va_end(arg_ptr);
+
+	buffer[MSG_BUF_LEN-1] = 0;
+
+	if (init_progress < 3)
+	{
+		fprintf(stderr, "\nFATAL ERROR: %s", buffer);
+	}
+
+	if (init_progress >= 3)
+	{
+// FIXME		DLG_ShowError("%s", buffer);
+
+		init_progress = 2;
+
+		Main_CloseWindow();
+	}
+#ifdef WIN32
+	else
+	{
+		MessageBox(NULL, buffer, "Silage : Error",
+		           MB_ICONEXCLAMATION | MB_OK |
+				   MB_SYSTEMMODAL | MB_SETFOREGROUND);
+	}
+#endif
+
+	init_progress = 0;
+
+	exit(2);
+}
+
+
+static void Main_OpenWindow()
+{
+	/*
+	 *  Create the window
+	 */
+	Fl::visual(FL_RGB);
+
+
+	Fl::background(236, 232, 228);
+	Fl::background2(255, 255, 255);
+	Fl::foreground(0, 0, 0);
+
+	Fl::scheme("plastic");
+
+
+#ifdef UNIX
+	Fl_File_Icon::load_system_icons();
+#endif
+
+
+	int screen_w = Fl::w();
+	int screen_h = Fl::h();
+
+	DebugPrintf("Detected Screen Size: %dx%d\n", screen_w, screen_h);
+
+
+	main_win = new UI_MainWin();
+
+	main_win->label("Silage v" VERSION);
+
+	// show window (pass some dummy arguments)
+	{
+		int   argc = 1;
+		char *argv[2];
+
+		argv[0] = StringDup("Silage.exe");
+		argv[1] = NULL;
+
+		main_win->show(argc, argv);
+	}
+
+	// kill the stupid bright background of the "plastic" scheme
+	{
+		delete Fl::scheme_bg_;
+		Fl::scheme_bg_ = NULL;
+
+		main_win->image(NULL);
+	}
+
+
+    Fl::add_handler(Main_key_handler);
+}
+
+
+static void Main_CloseWindow()
+{
+}
+
+
+/* ----- user information ----------------------------- */
+
+static void ShowHelp()
+{
+	printf(	"\n"
+			"*** Silage v" VERSION " ***\n"
+			"\n");
+
+	fflush(stdout);
+}
+
+
+/*
+ *  the driving program
+ */
+int main(int argc, char *argv[])
+{
+	init_progress = 0;
+
+	int r;
+
+	/* TODO: argument handling */
+
+	if (argc >= 2 &&
+	    ( strcmp(argv[1], "/?") == 0 ||
+	      strcmp(argv[1], "-h") == 0 ||
+	      strcmp(argv[1], "-help") == 0 ||
+	      strcmp(argv[1], "--help") == 0 ||
+	      strcmp(argv[1], "-version") == 0 ||
+	      strcmp(argv[1], "--version") == 0 ))
+	{
+		ShowHelp();
+		return 0;
+	}
+
+
+	init_progress = 1;
+
+
+	Main_OpenWindow();
+
+	init_progress = 3;
+
+
+    while (! want_quit)
+    {
+        Fl::wait(0.2);
+    }
+
+
+quit:
+	/* that's all folks! */
+
+
+	init_progress = 2;
+
+	Main_CloseWindow();
+
+
+	init_progress = 0;
+
+	return 0;
+}
+
+
+//--- editor settings ---
+// vi:ts=4:sw=4:noexpandtab
---
+++ misc/silage/main.h
@@ -0,0 +1,56 @@
+//------------------------------------------------------------------------
+//  MAIN DEFINITIONS
+//------------------------------------------------------------------------
+
+#ifndef __MAIN_H__
+#define __MAIN_H__
+
+
+#define SILAGE_VERSION  "0.51"
+
+/*
+ *  Standard headers
+ */
+
+#ifdef WIN32
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+#endif
+
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
+#include <ctype.h>
+#include <limits.h>
+#include <errno.h>
+#include <math.h>
+
+#ifndef WIN32
+#include <unistd.h>
+#endif
+
+#include <vector>
+
+
+/*
+ *  Additional headers
+ */
+
+// #include "sys_type.h"
+// #include "sys_macro.h"
+
+#include "window.h"
+
+
+/*
+ *  Exports from main.cc
+ */
+void FatalError(const char *fmt, ...);
+
+
+#endif  /* __MAIN_H__ */
+
+//--- editor settings ---
+// vi:ts=4:sw=4:noexpandtab
