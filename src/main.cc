2db6f597fb52379749d140def7ca08c6a9ade511
Andrew Apted <ajapted@users.sf.net>
2015-01-05 08:31:27 +0000

    Updated some copyright messages (e.g. About box) for 2015.

--- src/main.cc
+++ src/main.cc
@@ -873,7 +873,7 @@ bool Main_ProjectSetup(bool is_startup)
 static void ShowHelp()
 {
 	printf(	"\n"
-			"*** " EUREKA_TITLE " v" EUREKA_VERSION " (C) 2014 Andrew Apted, et al ***\n"
+			"*** " EUREKA_TITLE " v" EUREKA_VERSION " (C) 2015 Andrew Apted, et al ***\n"
 			"\n");
 
 	printf(	"Eureka is free software, under the terms of the GNU General\n"
@@ -965,7 +965,7 @@ int main(int argc, char *argv[])
 
 
 	LogPrintf("\n");
-	LogPrintf("*** " EUREKA_TITLE " v" EUREKA_VERSION " (C) 2014 Andrew Apted, et al ***\n");
+	LogPrintf("*** " EUREKA_TITLE " v" EUREKA_VERSION " (C) 2015 Andrew Apted, et al ***\n");
 	LogPrintf("\n");
 
 	// Sanity checks (useful when porting).
