ff5c3e87cbfc1015d3951521bef476207429244c
Andrew Apted <ajapted@users.sf.net>
2015-01-11 09:21:21 +0000

    Find/Replace : created main UI for this (done in Fluid, code reformatted and pasted here).  Nothing works yet....

--- src/ui_replace.cc
+++ src/ui_replace.cc
@@ -27,29 +27,72 @@ UI_FindAndReplace::UI_FindAndReplace(int X, int Y, int W, int H) :
 {
 	box(FL_FLAT_BOX);
 
-
 	Fl_Box *title = new Fl_Box(X + 50, Y + 10, W - 60, 30, "Find and Replace");
 	title->align(FL_ALIGN_LEFT | FL_ALIGN_INSIDE);
 	title->labelsize(18+KF*4);
 
 
-	Y += 50;
-	H -= 50;
-
-	X += 6;
-	W -= 12;
-
-	int MX = X + W/2;
+	what = new Fl_Choice(X+70, Y+55, 150, 25);
 
 
-	before = new Fl_Input(X + 80, Y, W - 100, 25, "Find: ");
-
-	Y += 30;
+	/* ---- FIND AREA ---- */
 
 
-	after  = new Fl_Input(X + 80, Y, W - 100, 25, "Replace: ");
+	{ find_match = new Fl_Input(X+70, Y+95, 127, 25, "Match: ");
+	}
+	{ find_choose = new Fl_Button(X+210, Y+95, 70, 25, "Choose");
+	}
+	{ find_desc = new Fl_Output(X+70, Y+125, 210, 25, "Desc: ");
+	}
+	{ find_but = new Fl_Button(X+50, Y+165, 80, 30, "Find");
+		find_but->labelfont(1);
+	}
+	{ select_all_but = new Fl_Button(X+165, Y+165, 93, 30, "Select All");
+	}
 
-	Y += 30;
+
+	/* ---- REPLACE AREA ---- */
+
+	{ rep_toggle = new Fl_Toggle_Button(X+15, Y+220, 160, 30, "Replace Info");
+		rep_toggle->labelsize(16);
+	} // Fl_Light_Button* replace_toggle
+
+	Fl_Group *rep_group = new Fl_Group(X+50, Y+260, 230, 95);
+	{
+		{ rep_value = new Fl_Output(X+70, Y+260, 127, 25, "New: ");
+		}
+		{ rep_choose = new Fl_Button(X+210, Y+260, 70, 25, "Choose");
+		}
+		{ rep_desc = new Fl_Output(X+70, Y+290, 210, 25, "Desc: ");
+		}
+		{ apply_but = new Fl_Button(X+50, Y+325, 80, 30, "Apply");
+			apply_but->labelfont(1);
+		}
+		{ replace_all_but = new Fl_Button(X+160, Y+325, 105, 30, "Replace All");
+		}
+
+	}
+	rep_group->end();
+
+
+	/* ---- FILTER AREA ---- */
+
+	{ filter_toggle = new Fl_Toggle_Button(X+15, Y+380, 100, 30, "Filters");
+		filter_toggle->labelsize(16);
+	}
+
+	filter_group = new Fl_Group(X+35, Y+415, 200, 75);
+	{
+		{ only_floors = new Fl_Round_Button(X+35, Y+415, 145, 25, " Only Floors");
+			only_floors->down_box(FL_ROUND_DOWN_BOX);
+		}
+		{ only_ceilings = new Fl_Round_Button(X+35, Y+435, 165, 30, " Only Ceilings");
+			only_ceilings->down_box(FL_ROUND_DOWN_BOX);
+		}
+		{ tag_match = new Fl_Input(X+105, Y+466, 130, 24, "Tag Match:");
+		}
+	}
+	filter_group->end();
 
 
 	resizable(NULL);
--- src/ui_replace.h
+++ src/ui_replace.h
@@ -25,8 +25,40 @@
 class UI_FindAndReplace : public Fl_Group
 {
 private:
-	Fl_Input *before;
-	Fl_Input *after;
+	// main thing we are finding / replacing
+	Fl_Choice *what;
+
+	// --- FIND AREA ---
+
+	Fl_Input  *find_match;
+	Fl_Button *find_choose;
+	Fl_Output *find_desc;
+	Fl_Button *find_but;
+	Fl_Button *select_all_but;
+
+	// --- REPLACE AREA ---
+
+	Fl_Toggle_Button *rep_toggle;
+	Fl_Group *rep_group;
+
+	Fl_Output *rep_value;
+	Fl_Button *rep_choose;
+	Fl_Output *rep_desc;
+	Fl_Button *apply_but;
+	Fl_Button *replace_all_but;
+
+	// --- FILTER AREA ---
+
+	Fl_Toggle_Button *filter_toggle;
+	Fl_Group *filter_group;
+
+	// FIXME: stuff for Things, LineDefs
+
+	// sector filters
+	Fl_Round_Button *only_floors;
+	Fl_Round_Button *only_ceilings;
+
+	Fl_Input *tag_match;
 
 public:
 	UI_FindAndReplace(int X, int Y, int W, int H);
