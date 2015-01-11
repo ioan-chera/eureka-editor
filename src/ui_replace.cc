938cb920de06ebfda072480cf15f6130703637e1
Andrew Apted <ajapted@users.sf.net>
2015-01-11 09:51:35 +0000

    Find/Replace : worked on layout some more, grouping the three sub-panels into their own group (leaving a think BG_COLOR border between each panel).  Also tidied up the code (the output from fluid).

--- src/ui_replace.cc
+++ src/ui_replace.cc
@@ -27,75 +27,88 @@ UI_FindAndReplace::UI_FindAndReplace(int X, int Y, int W, int H) :
 {
 	box(FL_FLAT_BOX);
 
-	Fl_Box *title = new Fl_Box(X + 50, Y + 10, W - 60, 30, "Find and Replace");
-	title->align(FL_ALIGN_LEFT | FL_ALIGN_INSIDE);
-	title->labelsize(18+KF*4);
+	color(WINDOW_BG, WINDOW_BG);
 
-
-	what = new Fl_Choice(X+70, Y+55, 150, 25);
-
-
+	
 	/* ---- FIND AREA ---- */
 
+	Fl_Group *grp1 = new Fl_Group(X, Y, W, 210);
+	grp1->box(FL_UP_BOX);
+	{
+		Fl_Box *title = new Fl_Box(X + 50, Y + 10, W - 60, 30, "Find and Replace");
+		title->align(FL_ALIGN_LEFT | FL_ALIGN_INSIDE);
+		title->labelsize(18+KF*4);
 
-	{ find_match = new Fl_Input(X+70, Y+95, 127, 25, "Match: ");
+
+		what = new Fl_Choice(X+70, Y+55, 150, 25);
+
+
+		find_match = new Fl_Input(X+70, Y+95, 127, 25, "Match: ");
+
+		find_choose = new Fl_Button(X+210, Y+95, 70, 25, "Choose");
+
+		find_desc = new Fl_Output(X+70, Y+125, 210, 25, "Desc: ");
+
+		find_but = new Fl_Button(X+50, Y+165, 80, 30, "Find");
+		find_but->labelfont(FL_HELVETICA_BOLD);
+
+		select_all_but = new Fl_Button(X+165, Y+165, 93, 30, "Select All");
 	}
-	{ find_choose = new Fl_Button(X+210, Y+95, 70, 25, "Choose");
-	}
-	{ find_desc = new Fl_Output(X+70, Y+125, 210, 25, "Desc: ");
-	}
-	{ find_but = new Fl_Button(X+50, Y+165, 80, 30, "Find");
-		find_but->labelfont(1);
-	}
-	{ select_all_but = new Fl_Button(X+165, Y+165, 93, 30, "Select All");
-	}
+	grp1->end();
 
 
 	/* ---- REPLACE AREA ---- */
 
-	{ rep_toggle = new Fl_Toggle_Button(X+15, Y+220, 160, 30, "Replace Info");
-		rep_toggle->labelsize(16);
-	} // Fl_Light_Button* replace_toggle
-
-	Fl_Group *rep_group = new Fl_Group(X+50, Y+260, 230, 95);
+	Fl_Group *grp2 = new Fl_Group(X, Y + 214, W, 156);
+	grp2->box(FL_UP_BOX);
 	{
-		{ rep_value = new Fl_Output(X+70, Y+260, 127, 25, "New: ");
+		rep_toggle = new Fl_Toggle_Button(X+15, Y+220, 160, 30, "Replace Info");
+		rep_toggle->labelsize(16);
+
+		Fl_Group *rep_group = new Fl_Group(X+50, Y+260, 230, 95);
+		{
+			rep_value = new Fl_Output(X+70, Y+260, 127, 25, "New: ");
+
+			rep_choose = new Fl_Button(X+210, Y+260, 70, 25, "Choose");
+
+			rep_desc = new Fl_Output(X+70, Y+290, 210, 25, "Desc: ");
+
+			apply_but = new Fl_Button(X+50, Y+325, 80, 30, "Apply");
+			apply_but->labelfont(FL_HELVETICA_BOLD);
+
+			replace_all_but = new Fl_Button(X+160, Y+325, 105, 30, "Replace All");
 		}
-		{ rep_choose = new Fl_Button(X+210, Y+260, 70, 25, "Choose");
-		}
-		{ rep_desc = new Fl_Output(X+70, Y+290, 210, 25, "Desc: ");
-		}
-		{ apply_but = new Fl_Button(X+50, Y+325, 80, 30, "Apply");
-			apply_but->labelfont(1);
-		}
-		{ replace_all_but = new Fl_Button(X+160, Y+325, 105, 30, "Replace All");
-		}
+		rep_group->end();
 
 	}
-	rep_group->end();
+	grp2->end();
 
 
 	/* ---- FILTER AREA ---- */
 
-	{ filter_toggle = new Fl_Toggle_Button(X+15, Y+380, 100, 30, "Filters");
-		filter_toggle->labelsize(16);
-	}
-
-	filter_group = new Fl_Group(X+35, Y+415, 200, 75);
+	Fl_Group *grp3 = new Fl_Group(X, Y + 374, W, H - 374);
+	grp3->box(FL_UP_BOX);
 	{
-		{ only_floors = new Fl_Round_Button(X+35, Y+415, 145, 25, " Only Floors");
+		filter_toggle = new Fl_Toggle_Button(X+15, Y+380, 100, 30, "Filters");
+		filter_toggle->labelsize(16);
+
+		filter_group = new Fl_Group(X+35, Y+415, 200, 75);
+		{
+			only_floors = new Fl_Round_Button(X+35, Y+415, 145, 25, " Only Floors");
 			only_floors->down_box(FL_ROUND_DOWN_BOX);
-		}
-		{ only_ceilings = new Fl_Round_Button(X+35, Y+435, 165, 30, " Only Ceilings");
+
+			only_ceilings = new Fl_Round_Button(X+35, Y+435, 165, 30, " Only Ceilings");
 			only_ceilings->down_box(FL_ROUND_DOWN_BOX);
+
+			tag_match = new Fl_Input(X+105, Y+466, 130, 24, "Tag Match:");
 		}
-		{ tag_match = new Fl_Input(X+105, Y+466, 130, 24, "Tag Match:");
-		}
+		filter_group->end();
 	}
-	filter_group->end();
+	grp3->end();
 
 
-	resizable(NULL);
+	grp3->resizable(NULL);
+	resizable(grp3);
 
 	end();
 }
