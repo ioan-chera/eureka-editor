e6214230a578c127d12550d6aa7a865414f3c9bd
Andrew Apted <ajapted@users.sf.net>
2016-01-24 21:03:17 +1100

    Version bumped,

--- src/main.h
+++ src/main.h
@@ -4,7 +4,7 @@
 //
 //  Eureka DOOM Editor
 //
-//  Copyright (C) 2001-2015 Andrew Apted
+//  Copyright (C) 2001-2016 Andrew Apted
 //  Copyright (C) 1997-2003 André Majorel et al
 //
 //  This program is free software; you can redistribute it and/or
@@ -30,7 +30,7 @@
 
 #define EUREKA_TITLE  "Eureka DOOM Editor"
 
-#define EUREKA_VERSION  "1.10"
+#define EUREKA_VERSION  "1.11"
 
 #define EUREKA_LUMP  "__EUREKA"
 
