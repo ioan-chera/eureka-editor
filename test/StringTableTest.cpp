91b8325085f691e50edcacf673d0fc1054ce2822
Ioan Chera <ioan.chera@gmail.com>
2020-12-18 22:25:34 +0200

    Added unit testing for the StringTable class

---
+++ test/StringTableTest.cpp
@@ -0,0 +1,33 @@
+//------------------------------------------------------------------------
+//
+//  Eureka DOOM Editor
+//
+//  Copyright (C) 2020 Ioan Chera
+//
+//  This program is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU General Public License
+//  as published by the Free Software Foundation; either version 2
+//  of the License, or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//------------------------------------------------------------------------
+
+#include "m_strings.h"
+
+#include "gtest/gtest.h"
+
+TEST(StringTable, Test)
+{
+    StringTable table;
+    int index = table.add("Jackson");
+    ASSERT_EQ(table.get(index), "Jackson");
+    int index2 = table.add("Jackson");
+    ASSERT_EQ(index2, index);
+    int index3 = table.add("Michael");
+    ASSERT_NE(index3, index2);
+    ASSERT_EQ(table.get(index3), "Michael");
+}
