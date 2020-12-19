147ab968b4ef1168f3142122662709c97a2a15ca
Ioan Chera <ioan.chera@gmail.com>
2020-12-19 19:24:34 +0200

    Added more SString tests

--- test/SStringTest.cpp
+++ test/SStringTest.cpp
@@ -46,6 +46,37 @@ TEST(SString, Test)
     ASSERT_TRUE(nullStringSized.empty());
 }
 
+TEST(SString, FindSpace)
+{
+    ASSERT_EQ(SString("Michael Jackson").findSpace(), 7);
+    ASSERT_EQ(SString("Michael \tJackson").findSpace(), 7);
+    ASSERT_EQ(SString("Michae\n \tJackson").findSpace(), 6);
+    ASSERT_EQ(SString("Micha\t \nJackson").findSpace(), 5);
+    ASSERT_EQ(SString("Mich\r \tJackson").findSpace(), 4);
+    ASSERT_EQ(SString("MichJackson").findSpace(), std::string::npos);
+    ASSERT_EQ(SString("").findSpace(), std::string::npos);
+    ASSERT_EQ(SString("\t").findSpace(), 0);
+}
+
+TEST(SString, TrimLeadingSpaces)
+{
+    SString string = " \t\t\nMichael  ";
+    string.trimLeadingSpaces();
+    ASSERT_EQ(string, "Michael  ");
+
+    SString emptyString = "";
+    emptyString.trimLeadingSpaces();
+    ASSERT_TRUE(emptyString.empty());
+
+    SString invariant = "Jackson  ";
+    invariant.trimLeadingSpaces();
+    ASSERT_EQ(invariant, "Jackson  ");
+
+    SString spaceOnly = "  \t\n\r  ";
+    spaceOnly.trimLeadingSpaces();
+    ASSERT_TRUE(spaceOnly.empty());
+}
+
 TEST(SString, TrimTrailingSet)
 {
     SString string = "//\\//Maglev//\\\\///";
@@ -59,4 +90,23 @@ TEST(SString, TrimTrailingSet)
     SString fullRemoval = "jackson";
     fullRemoval.trimTrailingSet("acjknos");
     ASSERT_TRUE(fullRemoval.empty());
+}
+
+TEST(SString, TrimTrailingSpaces)
+{
+    SString string = " \t\t\nMichael \t\n\r";
+    string.trimTrailingSpaces();
+    ASSERT_EQ(string, " \t\t\nMichael");
+
+    SString emptyString = "";
+    emptyString.trimTrailingSpaces();
+    ASSERT_TRUE(emptyString.empty());
+
+    SString invariant = "  Jackson";
+    invariant.trimTrailingSpaces();
+    ASSERT_EQ(invariant, "  Jackson");
+
+    SString spaceOnly = "  \t\n\r  ";
+    spaceOnly.trimTrailingSpaces();
+    ASSERT_TRUE(spaceOnly.empty());
 }
