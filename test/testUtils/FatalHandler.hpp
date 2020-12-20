994569182b18b39c19308668d01ebaab5b7255eb
Ioan Chera <ioan.chera@gmail.com>
2020-12-20 12:05:02 +0200

    Added utility code for the tests

---
+++ test/testUtils/FatalHandler.hpp
@@ -0,0 +1,42 @@
+//------------------------------------------------------------------------
+//
+//  Eureka DOOM Editor
+//
+//  Copyright (C) 2020 Ioan Chera
+//
+//  This program is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU General Public License
+//  as published by the Free Software Foundation; either version 2
+//  of the License, or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//------------------------------------------------------------------------
+
+#ifndef FatalHandler_hpp
+#define FatalHandler_hpp
+
+#include <iostream>
+#include <stdexcept>
+
+//
+// Handles a runtime_error exception fatally. Needed for death tests.
+//
+template<typename Callable>
+void Fatal(const Callable &callable)
+{
+	try
+	{
+		callable();
+	}
+	catch(const std::runtime_error &e)
+	{
+		std::cerr << e.what() << std::endl;
+		abort();
+	}
+}
+
+#endif /* FatalHandler_hpp */
