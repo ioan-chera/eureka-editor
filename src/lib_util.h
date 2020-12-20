8054336cf2bf81bde9e18d7262e42714d1b5e906
Ioan Chera <ioan.chera@gmail.com>
2020-12-20 23:19:00 +0200

    Deleted more dead code

--- src/lib_util.h
+++ src/lib_util.h
@@ -52,95 +52,12 @@ double AlongDist(double x, double y, /* coord to test */
 int RoundPOW2(int x);
 
 /*
- *  dectoi
- *  If <c> is a decimal digit ("[0-9]"), return its value.
- *  Else, return a negative number.
- */
-inline int dectoi (char c)
-{
-  if (isdigit ((unsigned char) c))
-    return c - '0';
-  else
-    return -1;
-}
-
-
-/*
- *  hextoi
- *  If <c> is a hexadecimal digit ("[0-9A-Fa-f]"), return its value.
- *  Else, return a negative number.
- */
-inline int hextoi (char c)
-{
-  if (isdigit ((unsigned char) c))
-    return c - '0';
-  else if (c >= 'a' && c <= 'f')
-    return c - 'a' + 10;
-  else if (c >= 'A' && c <= 'F')
-    return c - 'A' + 10;
-  else
-    return -1;
-}
-
-
-/*
  *  y_isident - return true iff <c> is one of a-z, A-Z, 0-9 or "_".
  *
  *  Intentionally not using isalpha() and co. because I
  *  don't want the results to depend on the locale.
  */
 #define IDENT_SET "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_"
-
-inline bool y_isident (char c)
-{
-  switch (c)
-  {
-    case 'a': case 'b': case 'c': case 'd': case 'e': case 'f':
-    case 'g': case 'h': case 'i': case 'j': case 'k': case 'l':
-    case 'm': case 'n': case 'o': case 'p': case 'q': case 'r':
-    case 's': case 't': case 'u': case 'v': case 'w': case 'x':
-    case 'y': case 'z':
-
-    case 'A': case 'B': case 'C': case 'D': case 'E': case 'F':
-    case 'G': case 'H': case 'I': case 'J': case 'K': case 'L':
-    case 'M': case 'N': case 'O': case 'P': case 'Q': case 'R':
-    case 'S': case 'T': case 'U': case 'V': case 'W': case 'X':
-    case 'Y': case 'Z':
-
-    case '0': case '1': case '2': case '3': case '4': case '5':
-    case '6': case '7': case '8': case '9':
-
-    case '_':
-
-      return true;
-
-    default:
-
-      return false;
-  }
-}
-
-
-/*
- *  round_up
- *  Round a value up to the next multiple of quantum.
- *
- *  Both the value and the quantum are supposed to be positive.
- */
-inline void round_up (int& value, int quantum)
-{
-  value = ((value + quantum - 1) / quantum) * quantum;
-}
-
-
-/*
- *  y_isprint
- *  Is <c> a printable character in ISO-8859-1 ?
- */
-inline bool y_isprint (char c)
-{
-  return (c & 0x60) && (c != 0x7f);
-}
 
 #endif  /* __EUREKA_YUTIL_H__ */
 
