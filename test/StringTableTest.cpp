b363ee5173be3b84bec5ea293c93d274d4a6c159
Ioan Chera <ioan.chera@gmail.com>
2022-07-01 18:49:46 +0300

    Added unit test for StringID

--- test/StringTableTest.cpp
+++ test/StringTableTest.cpp
@@ -20,6 +20,45 @@
 
 #include "gtest/gtest.h"
 
+TEST(StringID, Test)
+{
+	StringID sid;
+	ASSERT_EQ(sid.get(), 0);
+	ASSERT_TRUE(!sid);
+	ASSERT_TRUE(sid.isValid());
+	ASSERT_FALSE(sid.isInvalid());
+	ASSERT_FALSE(sid.hasContent());
+
+	StringID sid2;
+	ASSERT_EQ(sid, sid2);
+
+	sid = StringID(100);
+	ASSERT_NE(sid, sid2);
+
+	ASSERT_EQ(sid.get(), 100);
+	ASSERT_FALSE(!sid);
+	ASSERT_TRUE(sid.isValid());
+	ASSERT_FALSE(sid.isInvalid());
+	ASSERT_TRUE(sid.hasContent());
+
+	sid2 = sid;
+	ASSERT_EQ(sid, sid2);
+
+	sid = StringID(30);
+	sid2 = StringID(30);
+	ASSERT_EQ(sid, sid2);
+
+	sid = StringID(-31);
+	sid2 = StringID(-32);
+	ASSERT_NE(sid, sid2);
+	ASSERT_LT(sid.get(), 0);
+	ASSERT_LT(sid2.get(), 0);
+	ASSERT_FALSE(!sid);
+	ASSERT_FALSE(sid.isValid());
+	ASSERT_TRUE(sid.isInvalid());
+	ASSERT_FALSE(sid.hasContent());
+}
+
 TEST(StringTable, Test)
 {
     StringTable table;
