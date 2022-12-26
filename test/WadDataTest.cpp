5e3ff4a3aac276413c8d6242bb295bc82681533a
Ioan Chera <ioan.chera@gmail.com>
2022-12-26 14:24:51 +0200

    Added a findGlobalLump test

--- test/WadDataTest.cpp
+++ test/WadDataTest.cpp
@@ -20,6 +20,62 @@
 #include "w_wad.h"
 #include "gtest/gtest.h"
 
+//
+// Add a valid lump with content so it can be part of the namespace
+//
+static void addValidLump(const std::shared_ptr<Wad_file> &wad, const char *name)
+{
+	Lump_c *lump = wad->AddLump(name);
+	ASSERT_TRUE(lump);
+	lump->Printf("a");
+}
+
+TEST(MasterDir, FindGlobalLump)
+{
+	MasterDir master;
+	auto wad = Wad_file::Open("dummy.wad", WadOpenMode::write);
+	ASSERT_TRUE(wad);
+	addValidLump(wad, "LUMP1");	// 0
+	addValidLump(wad, "LUMP2");
+	addValidLump(wad, "S_START");
+	addValidLump(wad, "LUMP3");
+	addValidLump(wad, "LUMP4");	// 4
+	addValidLump(wad, "S_END");
+	addValidLump(wad, "LUMP5");
+	addValidLump(wad, "F_START");
+	addValidLump(wad, "LUMP6");	// 8
+	addValidLump(wad, "F_END");
+	addValidLump(wad, "LUMP7");
+
+	master.MasterDir_Add(wad);
+
+	auto wad2 = Wad_file::Open("dummy.wad", WadOpenMode::write);
+	ASSERT_TRUE(wad2);
+
+	addValidLump(wad2, "LUMP11");	// 0
+	addValidLump(wad2, "LUMP2");
+	addValidLump(wad2, "SS_START");
+	addValidLump(wad2, "LUMP5");
+	addValidLump(wad2, "S_END");	// 4
+	addValidLump(wad2, "LUMP3");
+	addValidLump(wad2, "FF_START");
+	addValidLump(wad2, "LUMP6");
+	addValidLump(wad2, "FF_END");	// 8
+	addValidLump(wad2, "LUMP7");
+
+	master.MasterDir_Add(wad2);
+
+	ASSERT_EQ(master.findGlobalLump("LUMP1"), wad->GetLump(0));
+	ASSERT_EQ(master.findGlobalLump("LUMP2"), wad2->GetLump(1));
+	ASSERT_EQ(master.findGlobalLump("LUMP3"), wad2->GetLump(5));
+	ASSERT_EQ(master.findGlobalLump("LUMP4"), nullptr);
+	ASSERT_EQ(master.findGlobalLump("LUMP5"), wad->GetLump(6));
+	ASSERT_EQ(master.findGlobalLump("LUMP6"), nullptr);
+	ASSERT_EQ(master.findGlobalLump("LUMP7"), wad2->GetLump(9));
+	ASSERT_EQ(master.findGlobalLump("LUMP11"), wad2->GetLump(0));
+	ASSERT_EQ(master.findGlobalLump("LUMP12"), nullptr);
+}
+
 TEST(MasterDir, FindFirstSpriteLump)
 {
 	MasterDir master;
