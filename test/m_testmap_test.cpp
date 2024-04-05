9f96b7a9eec3479e6793c03ef89998b39f7c32f9
Ioan Chera <ioan.chera@gmail.com>
2024-04-05 07:46:55 +0300

    Added Windows testmap testing
    * Added Windows testing
    * Found a weirdness on the POSIX code
    * Added spaces to paths to make sure we test properly

--- test/m_testmap_test.cpp
+++ test/m_testmap_test.cpp
@@ -30,20 +30,20 @@
 #include <unistd.h>
 #endif
 
+#include <chrono>
 #include <fstream>
+#include <thread>
 
-#ifdef _WIN32
-#else
-class TestMapFixturePOSIX : public TempDirContext
+class TestMapFixture : public TempDirContext
 {
 protected:
 	void SetUp() override;
-	void setPortName(const char *name);
+	void setPortName(const char* name);
 	void addIWAD();
 	void addResources();
 	void addPWAD();
 	std::vector<std::string> getResultLines() const;
-	
+
 	Instance inst;
 	fs::path outputPath;
 	fs::path portPath;
@@ -53,90 +53,116 @@ protected:
 	fs::path editWadPath;
 };
 
-void TestMapFixturePOSIX::SetUp()
+void TestMapFixture::SetUp()
 {
 	TempDirContext::SetUp();
 	outputPath = getChildPath("output.list");
-	
+
 	// Setup the program
+#ifdef _WIN32
+	portPath = getChildPath("port.bat");
+	std::ofstream stream(portPath);
+	ASSERT_TRUE(stream.is_open());
+	mDeleteList.push(portPath);
+	stream << "@echo off" << std::endl;
+	stream << "echo running script" << std::endl;
+	stream << "echo %0 > " << SString(outputPath.u8string()).spaceEscape(false) << std::endl;
+	stream << "for %%x in (%*) do (" << std::endl;
+	stream << "echo %%x >> " << SString(outputPath.u8string()).spaceEscape(false) << std::endl;
+	stream << ")" << std::endl;
+	stream.close();
+#else
 	portPath = getChildPath("port.sh");
 	std::ofstream stream(portPath);
 	ASSERT_TRUE(stream.is_open());
 	mDeleteList.push(portPath);
 	stream << "#!/bin/bash" << std::endl;
 	stream << "echo running script" << std::endl;
+	// TODO: fix this!?
 	stream << "echo \"$0\" > " << SString(portPath.u8string()).spaceEscape(true) << std::endl;
 	stream << "for var in \"$@\"" << std::endl;
 	stream << "do" << std::endl;
 	stream << "echo \"$var\" >> " << SString(outputPath).spaceEscape(true) <<
-	std::endl;
+		std::endl;
 	stream << "done" << std::endl;
 	stream.close();
 	int r = chmod(portPath.u8string().c_str(), S_IRWXU);
 	ASSERT_FALSE(r);
+#endif
 }
 
-void TestMapFixturePOSIX::setPortName(const char *name)
+
+void TestMapFixture::setPortName(const char* name)
 {
 	// Prepare the conditions
 	global::recent.setPortPath(!strcmp(name, "vanilla") ? "vanilla_doom2" : name, portPath);
-	
+
 	// Populate for GrabWadNamesArgs
 	inst.loaded.portName = name;
 }
 
-void TestMapFixturePOSIX::addIWAD()
+void TestMapFixture::addIWAD()
 {
-	gameWadPath = getChildPath("game.wad");
+	gameWadPath = getChildPath("ga me.wad");
 	std::shared_ptr<Wad_file> gameWad = Wad_file::Open(gameWadPath, WadOpenMode::write);
 	inst.wad.master.setGameWad(gameWad);
 }
 
-void TestMapFixturePOSIX::addResources()
+void TestMapFixture::addResources()
 {
 	std::vector<std::shared_ptr<Wad_file>> resources;
-	res1Path = getChildPath("res1.wad");
-	res2Path = getChildPath("res2.wad");
+	res1Path = getChildPath("re s1.wad");
+	res2Path = getChildPath("re s2.wad");
 	resources.push_back(Wad_file::Open(res1Path, WadOpenMode::write));
 	resources.push_back(Wad_file::Open(res2Path, WadOpenMode::write));
 	inst.wad.master.setResources(resources);
 }
 
-void TestMapFixturePOSIX::addPWAD()
+void TestMapFixture::addPWAD()
 {
-	editWadPath = getChildPath("edit.wad");
+	editWadPath = getChildPath("ed it.wad");
 	std::shared_ptr<Wad_file> editWad = Wad_file::Open(editWadPath, WadOpenMode::write);
 	inst.wad.master.ReplaceEditWad(editWad);
 }
 
-std::vector<std::string> TestMapFixturePOSIX::getResultLines() const
+std::vector<std::string> TestMapFixture::getResultLines() const
 {
 	// Try until it's open
-	usleep(200000);	// sleep at the beginning to wait for file to be completely written
+	// sleep at the beginning to wait for file to be completely written
+	std::this_thread::sleep_for(std::chrono::milliseconds(200));
 	std::ifstream input;
-	for(int i = 0; i < 20; ++i)
+	for (int i = 0; i < 20; ++i)
 	{
-		
 		input.open(outputPath);
-		if(input.is_open())
+		if (input.is_open())
 			break;
-		usleep(100000);	// retry
+		// retry
+		std::this_thread::sleep_for(std::chrono::milliseconds(100));
 	}
 	EXPECT_TRUE(input.is_open());
-	
+
 	std::vector<std::string> result;
-	while(!input.eof())
+	while (!input.eof())
 	{
 		std::string line;
 		std::getline(input, line);
-		if(line.empty() && input.eof())
+		if (line.empty() && input.eof())
 			return result;
+		// Windows may add quotes on arguments with spaces
+#ifdef _WIN32
+		SString changedLine = SString(line);
+		changedLine.trimTrailingSpaces();
+		line = changedLine.get();
+		if (line.length() >= 2 && line[0] == '"' && line.back() == '"')
+			line = line.substr(1, line.length() - 2);
+#endif
 		result.push_back(line);
 	}
 	return result;
 }
 
-TEST_F(TestMapFixturePOSIX, TestMapVanillaWithResources)
+
+TEST_F(TestMapFixture, TestMapVanillaWithResources)
 {
 	setPortName("vanilla");
 	
@@ -158,7 +184,7 @@ TEST_F(TestMapFixturePOSIX, TestMapVanillaWithResources)
 	ASSERT_EQ(lines, expected);
 }
 
-TEST_F(TestMapFixturePOSIX, TestMapPortWithResources)
+TEST_F(TestMapFixture, TestMapPortWithResources)
 {
 	setPortName("boom");
 	
@@ -180,7 +206,7 @@ TEST_F(TestMapFixturePOSIX, TestMapPortWithResources)
 	ASSERT_EQ(lines, expected);
 }
 
-TEST_F(TestMapFixturePOSIX, TestMapPortWithoutResources)
+TEST_F(TestMapFixture, TestMapPortWithoutResources)
 {
 	setPortName("boom");
 	
@@ -199,7 +225,7 @@ TEST_F(TestMapFixturePOSIX, TestMapPortWithoutResources)
 	ASSERT_EQ(lines, expected);
 }
 
-TEST_F(TestMapFixturePOSIX, TestMapPortWithoutResourcesDoom1Map)
+TEST_F(TestMapFixture, TestMapPortWithoutResourcesDoom1Map)
 {
 	setPortName("boom");
 	
@@ -218,7 +244,7 @@ TEST_F(TestMapFixturePOSIX, TestMapPortWithoutResourcesDoom1Map)
 	ASSERT_EQ(lines, expected);
 }
 
-TEST_F(TestMapFixturePOSIX, TestMapPortWithoutResourcesNonstandardMap)
+TEST_F(TestMapFixture, TestMapPortWithoutResourcesNonstandardMap)
 {
 	setPortName("boom");
 	
@@ -237,7 +263,7 @@ TEST_F(TestMapFixturePOSIX, TestMapPortWithoutResourcesNonstandardMap)
 	ASSERT_EQ(lines, expected);
 }
 
-TEST_F(TestMapFixturePOSIX, TestMapPortWithoutResourcesBadMap)
+TEST_F(TestMapFixture, TestMapPortWithoutResourcesBadMap)
 {
 	setPortName("boom");
 	
@@ -255,5 +281,3 @@ TEST_F(TestMapFixturePOSIX, TestMapPortWithoutResourcesBadMap)
 	std::vector<std::string> expected = {portPath.u8string(), "-iwad", gameWadPath.u8string(), "-file", editWadPath.u8string()};
 	ASSERT_EQ(lines, expected);
 }
-
-#endif
