86587099d948d659e8e10b5bb62e4ab49f4b2a8e
Ioan Chera <ioan.chera@gmail.com>
2024-05-01 22:58:44 +1200

    Also test DEHACKED lumps, additively

--- test/w_dehacked_test.cpp
+++ test/w_dehacked_test.cpp
@@ -18,8 +18,41 @@
 
 #include "w_dehacked.h"
 #include "m_game.h"
+#include "w_wad.h"
+#include "WadData.h"
 #include "testUtils/TempDirContext.hpp"
 #include "gtest/gtest.h"
+
+static const char *headerBoilerplate[] = {
+	"Patch file for Dehacked v3.1",
+	"# Here is the file",
+	""
+};
+
+static const char *part1[] = {
+	"Thing 2 (Imp Sarge Trooper Vanilla Shadow Ceiling Weapon)",
+	"Initial frame = 442",
+	"Width = 655360",
+	"ID # = 2222",
+	"Bits = 262400",
+	"#$Editor category = Weapons",
+	"",
+	"Frame 442",
+	"Sprite number = 31",
+	"Sprite subnumber = 32769",
+	"",
+	"Text 4 4",
+	"VILEABCD",
+	""
+};
+
+static const char *part2[] = {
+	"[SPRITES]",
+	"30 = NEWB",
+	"",
+	"Thing 3 (Newbie)",
+	"Bits = SPAWNCEILING+SHADOW"
+};
 
 class DehackedTest : public TempDirContext
 {
@@ -34,28 +67,12 @@ void DehackedTest::makeFile()
 	mDeleteList.push(getChildPath("tested.deh"));
 	
 	// Add some bogus headers
-	stream << "Patch file for Dehacked v3.1" << std::endl;
-	stream << "# Here is the file" << std::endl;
-	stream << std::endl;
-	stream << "Thing 2 (Imp Sarge Trooper Vanilla Shadow Ceiling Weapon)" << std::endl;
-	stream << "Initial frame = 442" << std::endl;
-	stream << "Width = 655360" << std::endl;
-	stream << "ID # = 2222" << std::endl;
-	stream << "Bits = 262400" << std::endl;
-	stream << "#$Editor category = Weapons" << std::endl;
-	stream << std::endl;
-	stream << "Frame 442" << std::endl;
-	stream << "Sprite number = 31" << std::endl;
-	stream << "Sprite subnumber = 32769" << std::endl;
-	stream << std::endl;
-	stream << "Text 4 4" << std::endl;
-	stream << "VILEABCD" << std::endl;
-	stream << std::endl;
-	stream << "[SPRITES]" << std::endl;
-	stream << "30 = NEWB" << std::endl;
-	stream << std::endl;
-	stream << "Thing 3 (Newbie)" << std::endl;
-	stream << "Bits = SPAWNCEILING+SHADOW" << std::endl;
+	for(const char *line : headerBoilerplate)
+		stream << line << std::endl;
+	for(const char *line : part1)
+		stream << line << std::endl;
+	for(const char *line : part2)
+		stream << line << std::endl;
 }
 
 TEST_F(DehackedTest, LoadNoFile)
@@ -64,10 +81,8 @@ TEST_F(DehackedTest, LoadNoFile)
 	ASSERT_FALSE(dehacked::loadFile("bogus.deh", config));
 }
 
-TEST_F(DehackedTest, LoadFile)
+static ConfigData baseConfig()
 {
-	makeFile();
-	
 	ConfigData config;
 	thingtype_t *type = &config.thing_types[3004];
 	type->group = 'm';
@@ -82,10 +97,12 @@ TEST_F(DehackedTest, LoadFile)
 	type->radius = 20.0f;
 	type->desc = "Sergeant";
 	type->sprite = "SPOS";
-	
-	bool result = dehacked::loadFile(getChildPath("tested.deh"), config);
-	ASSERT_TRUE(result);
-	
+
+	return config;
+}
+
+static void assertChangedConfig(const ConfigData &config)
+{
 	ASSERT_EQ(config.thing_types.find(3004), config.thing_types.end());
 	auto it = config.thing_types.find(2222);
 	ASSERT_NE(it, config.thing_types.end());
@@ -106,4 +123,42 @@ TEST_F(DehackedTest, LoadFile)
 	ASSERT_EQ(newtype->radius, 20.0f);
 	ASSERT_EQ(newtype->desc, "Newbie");
 	ASSERT_EQ(newtype->sprite, "NEWB");
+}
+
+TEST_F(DehackedTest, LoadFile)
+{
+	makeFile();
+	
+	ConfigData config = baseConfig();
+	
+	bool result = dehacked::loadFile(getChildPath("tested.deh"), config);
+	ASSERT_TRUE(result);
+	
+	assertChangedConfig(config);
+}
+
+TEST_F(DehackedTest, LoadLumps)
+{
+	auto wad = Wad_file::Open("test.wad", WadOpenMode::write);
+	ASSERT_TRUE(wad);
+	Lump_c &dhe1 = wad->AddLump("DEHACKED");
+	for(const char *line : part1)
+		dhe1.Printf("%s\n", line);
+	
+	std::vector<std::shared_ptr<Wad_file>> resources;
+	resources.push_back(wad);
+	
+	wad = Wad_file::Open("test2.wad", WadOpenMode::write);
+	resources.push_back(wad);
+	Lump_c &dhe2 = wad->AddLump("DEHACKED");
+	for(const char *line : part2)
+		dhe2.Printf("%s\n", line);
+	
+	MasterDir dir;
+	dir.setResources(resources);
+	
+	ConfigData config = baseConfig();
+	
+	dehacked::loadLumps(dir, config);
+	assertChangedConfig(config);
 }
